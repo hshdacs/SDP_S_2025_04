fff76264bed919d495e41b10f4e8ab9cd0e3291d
Applied Computer Science <peter.dillinger2@srh.de>
2025-04-24 11:50:27 +0200

    completed the Euclid program

---
+++ ex02/Euclid.java
@@ -0,0 +1,23 @@
+package ex02;
+
+public class Euclid {
+    
+    public static void main(String[] args) {
+        int x = -24;
+        int y = 16;
+
+        System.out.print("The GCD of " + x + " and " + y + " is ");        
+
+        if (x < 0) x = -x; // Math.abs(x)
+        if (y < 0) y = -y;
+
+        if (x == 0) x = y;
+        else if (y != 0) {
+            while (x != y) {
+                if (x > y) x = x - y;
+                else y = y - x;
+            }
+        }
+        System.out.println(x);        
+    }
+}

3a8a4e6fead00d208d895a9c3e4d0289b6f2e5bb
Applied Computer Science <peter.dillinger2@srh.de>
2025-05-22 14:37:04 +0200

    added a small okay button to the account list dialog

--- ex09/view/AccountListView.java
+++ ex09/view/AccountListView.java
@@ -1,8 +1,10 @@
 package ex09.view;
 
+import java.awt.BorderLayout;
 import java.awt.ScrollPane;
 
 import javax.swing.DefaultListModel;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JList;
 
@@ -13,10 +15,15 @@ public class AccountListView extends JFrame {
         setTitle("Account List");
         setSize(250, 400);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+        setLayout( new BorderLayout() );
         ScrollPane scrollPane = new ScrollPane();
-        scrollPane.add(accountList);
+        scrollPane.add(accountList, BorderLayout.CENTER);
         add(scrollPane);
 //        add(accountList);
+        JButton okayButton = new JButton("Okay");
+        add(okayButton, BorderLayout.SOUTH );
+        okayButton.addActionListener( e -> dispose() );
         pack();
         setVisible(true);
     }
